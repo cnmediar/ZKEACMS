c91a539046e1087d3279b3cd3d783f07e484caf1
mediar <mediar@flyingtop.cn>
2019-04-15 18:28:39 +0800

    HI

--- src/ZKEACMS.WebHost/Publish.cmd
+++ src/ZKEACMS.WebHost/Publish.cmd
@@ -1,2 +1,17 @@
-@echo off
-publish-zkeacms
+
+
+
+[Unit]
+Description=ZKEACMS
+
+[Service]
+WorkingDirectory=/home/wwwroot/asianlink/web
+ExecStart=/usr/bin/dotnet /home/wwwroot/asianlink/web/ZKEACMS.WebHost.dll
+Restart=always
+RestartSec=10
+SyslogIdentifier=zkeacms
+User=root
+Environment=ASPNETCORE_ENVIRONMENT=Production 
+
+[Install]
+WantedBy=multi-user.target
